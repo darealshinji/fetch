a06e5a61db3cdc0e18198ff858688779ec47d459
Marshall Banana <djcj@gmx.de>
2015-05-05 15:41:56 +0200

    Use automake to build everything

--- source/autogen.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-rm -rf autom4te.cache config.h.in config.log configure Makefile
-autoreconf -ivf
-rm -rf autom4te.cache
--- source/libnbcompat/autogen.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-rm -rf autom4te.cache config.h.in config.log configure
-autoreconf -ivf
-rm -rf autom4te.cache
